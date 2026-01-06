a79ed2332d729ec31069339dad44aa4d39d54019
BUTSURO <johndoe@example.com>
2026-01-07 01:59:10 +0400

    initial commit

--- src/components/WhyUsSection.tsx
+++ src/components/WhyUsSection.tsx
@@ -1,15 +1,13 @@
 const MerchSection = () => {
   return (
-    <section className="min-h-screen bg-black relative flex items-center overflow-hidden">
+    <section className="min-h-screen bg-black relative flex items-center overflow-hidden px-10">
 
-      <div className="w-full py-2">
+      <div className="w-full py-1">
         <div className="grid md:grid-cols-2 gap-12 items-center max-w-7xl mx-auto px-4">
           {/* Left Content */}
-          <div className="relative space-y-8">
-            {/* Diamond shape behind text - half off screen */}
-            
-            <div className="relative space-y-1">
-              <p className="text-white/60 text-sm tracking-wider uppercase">
+          <div className="relative space-y-8">            
+            <div className=" relative space-y-1">
+              <p className="text-white/60 text-sm tracking-wider md:pt-5">
                 Coming soon
               </p>
               
@@ -20,7 +18,7 @@ const MerchSection = () => {
                   alt="CPKY"
                   className="w-80 h-34 -ml-4"
                 />
-                <h1 className="mt-0 pb-20 text-7xl md:text-8xl font-serif text-white tracking-tight ml-10">
+                <h1 className="mt-0 pb-20 lg:text-7xl md:text-[3.2rem] sm:text-8xl font-serif text-white tracking-tight ml-10">
                   TANK TOP
                 </h1>
               </div>
